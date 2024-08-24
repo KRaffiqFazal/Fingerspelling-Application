09f0e723f36121983bb109c50b6098eb51cf8c96
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2024-08-24 10:37:25 +0100

    Added blazor project

---
+++ UserFacingService/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UserFacingService
+{
+  public partial class App : Application
+  {
+    public App()
+    {
+      InitializeComponent();
+
+      MainPage = new MainPage();
+    }
+  }
+}
---
+++ UserFacingService/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Manuela;
+using Manuela.WindowStyle;
+
+namespace UserFacingService
+{
+  public partial class MainPage : AppPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    protected override void OnAppLoaded(object? sender, EventArgs e)
+    {
+      SetStatusAndNavigationBarColors();
+      UpdatePointerPassthroughRegion();
+    }
+
+    private static void SetStatusAndNavigationBarColors()
+    {
+      ManuelaWindow.SetWindowColors(Color.FromArgb("#3a0647"), Color.FromArgb("#ffffff"));
+    }
+
+    private void UpdatePointerPassthroughRegion()
+    {
+      // declares a zone where the pointer events will pass through the title bar on windows
+      // lets allow the user to click on the toggle button
+      ManuelaWindow.SetPointerPassthroughRegion([new(0, 0, 80, 32)]);
+    }
+  }
+}
