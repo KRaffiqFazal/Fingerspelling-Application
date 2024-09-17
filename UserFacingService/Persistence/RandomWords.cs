eec96d60f12c49b593761d09690b363af911f16e
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2024-09-17 19:26:29 +0100

    Added random word generator nuget package.

---
+++ UserFacingService/Persistence/RandomWords.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace UserFacingService.Persistence;
+
+internal class RandomWords : IStringGeneration
+{
+    public string GenerateString(int charsLong)
+    {
+        throw new NotImplementedException();
+    }
+}
+
