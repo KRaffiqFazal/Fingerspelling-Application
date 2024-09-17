37bbc7c24183e7bb02f3a0796015e2db82702e81
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2024-09-17 19:15:59 +0100

    Created random string gen class.

---
+++ UserFacingService/Persistence/RandomChars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Google.Crypto.Tink.Internal;
+using Random = System.Random;
+
+namespace UserFacingService.Persistence;
+
+internal class RandomChars : IStringGeneration
+{
+    private const string c_alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public string GenerateString(int charsLong)
+    {
+        var stringOfChars = new StringBuilder(charsLong);
+
+        var randomGen = new Random();
+
+        for (var i = 1; i <= charsLong; i++)
+        {
+            stringOfChars.Append(c_alphabet[randomGen.Next(0, c_alphabet.Length - 1)]);
+        }
+
+        return stringOfChars.ToString();
+    }
+}
